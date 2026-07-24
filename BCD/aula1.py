0f99040eb636e734d3e291b513899b41f713bbe4
Keynan Santos <estudantekeynan@gmail.com>
2026-07-24 11:22:20 -0300

    Organizacao

---
+++ BCD/aula1.py
@@ -0,0 +1,12 @@
+# Clientes
+
+
+
+# CAMPO:  Registro:  ATRIBUTO:
+# nome: 	Keynan     STR
+# idade:	 16        INT
+# cidade:	 Limeira   
+# curso: 	ADS
+# hobby: 	Jogar
+# CPF:
+#A chave primária é o CPF, pois é um identificador único para cada cliente, mas ela tambem poderia ser um ID, pois cada cliente teria um ID único. O CPF é mais adequado, pois é um dado real e único para cada pessoa, enquanto o ID seria apenas um número gerado pelo sistema.
