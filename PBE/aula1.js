549f2fb239ea79fbd582894a2a52d578b1018a66
Keynan <estudantekeynan@gmail.com>
2026-07-24 16:55:18 -0300

    javascript

---
+++ PBE/aula1.js
@@ -0,0 +1,2 @@
+const readline = require('readline/promises');
+const {stdin: input, stdout: output} =require('process')
---
+++ PBE/testes.py
@@ -0,0 +1,58 @@
+# #Hello WORLD
+
+
+
+# #1-
+
+# # print ("Seja bem vindo a locadora de carros")
+
+
+# # aluguel = 90
+# # Preco_km = 0.20
+
+# # Dias = int(input("Quantos dias você ficou com o carro? \n"))
+# # KM_rodado = float(input("Quantos KM você rodou com o carro? "))
+
+# # if KM_rodado >= 100:
+# #  km = 0.15
+# #  resultado = (Dias * aluguel )+ (KM_rodado * km)
+# #  print(f" O valor a ser pago é {resultado}$")
+
+# # else:
+# #     valor_final =(Dias * aluguel )+ (KM_rodado * Preco_km)
+# #     print(F"O resultado é {valor_final}")
+
+
+
+
+
+
+# #2-
+# # numeros =[12, 5 , 8, 21, 14, 3, 10, 7]
+# # numero_par = []
+# # soma = 0
+# # n = 0
+
+
+# # for n in numeros:
+        
+# #     if n %2 == 0:
+# #         numero_par.append(n)
+# #         print(numero_par)
+    
+# #     media = sum(numero_par) / len(numero_par)
+# #     print(f" A media é {media :.2f}")
+    
+
+
+# # 3 -
+
+
+# produto = {"nome:""teclado mecanico","preco":200,"eestoque:" 15,"categoria": "Perifericos"}
+
+
+# def aplicar_desconto(item, porcentagem):
+#     item["categoria"] = item ["preco"]
+#     item ["preco"] -= item["preco"] * (porcentagem/100)
+#     print(f"O produto {item['nome']}agora custa {item['preco']:.2f}" e a categoria {item['categoria']})
+#     aplicar_desconto(produto,10)
