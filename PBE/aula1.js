0803e205a0328821f99c34218c978e4252f907bb
Keynan <estudantekeynan@gmail.com>
2026-08-06 10:51:50 -0300

    Ativida Formativa

--- PBE/aula1.js
+++ PBE/aula1.js
@@ -1,6 +1,9 @@
-const readline = require('readline/promises');
-const {stdin: input, stdout: output} =require('process')
+// const readline = require('readline/promises');
+// const {stdin: input, stdout: output} =require('process')
 
+console.log("Hello World");
+console.log("Estou começando a aprender JavaScript!");
+console.log("Esta é a disciplina de Backend 1.")
 
 
 
---
+++ PBE/desafio2.js
@@ -0,0 +1,8 @@
+const produto = "Caderno";
+const preco = 20;
+const quantidade = 3;
+const total = preco * quantidade
+
+console.log(`Produto:  ${produto}`);
+console.log(`Valor total a pagar: ${total}`);
+
